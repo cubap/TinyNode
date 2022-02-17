73f944c57cbd3529df69f8c1255f6066d1b42a53
cubap <cubap@slu.edu>
2022-02-17 14:38:42 -0600

    comments

--- tokens.js
+++ tokens.js
@@ -3,6 +3,11 @@ require('dotenv').config()
 // https://stackoverflow.com/a/69058154/1413302
 const isTokenExpired = (token) => (Date.now() >= JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString()).exp * 1000)
 
+/**
+ * Use the privately stored refresh token to generate a new access token for
+ * your RERUM-connected application. There is no way to authenticate this 
+ * process, so protect your refresh token and replace it if it is exposed. 
+ */
 function generateNewAccessToken() {
     const payload = {
         methdo: 'POST',
@@ -20,5 +25,9 @@ function generateNewAccessToken() {
     console.warn("Access Token expired. Consider updating your .env files")
 }
 
-// run once onload
+/**
+ * This will conduct a simple check against the expiry date in your token.
+ * This does not validate your access token, so you may still be rejected by 
+ * your RERUM instance as unauthorized.
+ */
 if (isTokenExpired(process.env.access_token)) { generateNewAccessToken() }

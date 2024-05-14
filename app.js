d064a23640515234f87c763154c9e61bd3ffcaf5
Patrick Cuba <cubap@slu.edu>
2024-05-14 12:31:35 -0500

    fixes #51 - move dotenv

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
-var dotenv = require('dotenv')
-var dotenvExpand = require('dotenv-expand')
-var storedEnv = dotenv.config()
-dotenvExpand.expand(storedEnv)
 
 require('./tokens.js')
 

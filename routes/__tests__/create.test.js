06707aafa315d0158e2847499b52e3a0f34cee3f
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2024-05-23 16:29:08 -0500

    Notes and demo

--- routes/__tests__/create.test.js
+++ routes/__tests__/create.test.js
@@ -2,9 +2,48 @@ import express from "express"
 import request from "supertest"
 import createRoute from "../create.js"
 import app from "../../app.js"
-
+import {createRequest, createResponse} from 'node-mocks-http'
 const routeTester = new express()
 routeTester.use("/create", createRoute)
+
+//wrap these separately as their own suite, not necessarily another file
+
+/**
+ * To run just the mocks, do npm run mocks
+ * Mock lib: https://github.com/eugef/node-mocks-http
+ * 
+ * Remember to actually test create.js.
+ * This test should fail if create.js fails its basic request processings and response building.
+ * 
+ * This test runs the logic of the route file 'create.js' but does not actually communicate with RERUM.
+ * 
+ *   Then what is tested exacly in create.js by this test?
+ *   - Is the express req/resp sent into the route
+ *   - Can the route read the JSON body
+ *   - Does the route add @id and __rerum
+ *   - Does the route respond 201
+ *   - Does the route respond with the object that was in the request body
+ *   - Does the route respond with a 'Location' header
+ */ 
+
+describe("Playing with a successful mock create.  __mockit", () => {
+  it("'/create' route request and response behavior is functioning.", () => {
+    /** Request Mock */
+    let mock_request = createRequest({
+      method: "POST",
+      url: "/",
+      body: {"test":"item"}  
+    })
+    let mock_response = createResponse()
+    createRoute(mock_request, mock_response)
+    const data = mock_response._getJSONData() 
+    console.log(mock_response._getHeaders())
+    expect(mock_response._getHeaders()).toHaveProperty('location')
+    expect(mock_response.statusCode).toBe(201)
+    expect(data["test"]).toBe("item")
+  })
+})
+
 
 describe("Combined unit tests for the '/create' route.", () => {
   it("'/app/create' and '/create' are registered routes in the app.  __exists __core", () => {
@@ -117,6 +156,8 @@ describe("Combined unit tests for the '/create' route.", () => {
     .catch(err => err)
   })
 
+  // This is a full integration test that we need that should fail when RERUM is down
+  
   // TODO: Can we avoid creating an object
   it("'/create' route can save an object to RERUM.  __e2e", () => {
     request(routeTester)
@@ -125,7 +166,7 @@ describe("Combined unit tests for the '/create' route.", () => {
     .then(response => {
       expect(response.header).toHaveProperty('location')
       expect(response.statusCode).toBe(201)
-      expect(response.body.test.toBe("item"))
+      expect(response.body.test).toBe("item")
     })
     .catch(err => err)
   })
--- routes/create.js
+++ routes/create.js
@@ -3,10 +3,12 @@ const router = express.Router()
 
 /* POST a create to the thing. */
 router.post('/', async (req, res, next) => {
-
   try {
-    // check body for JSON
     const body = JSON.stringify(req.body)
+    // Make a valid looking result to use for testing.
+    let result = JSON.parse(JSON.stringify(req.body))
+    result["@id"] = "https://store.rerum.io/v1/id/_not_"
+    result["__rerum"] = {"stuff":"here"}
     const createOptions = {
       method: 'POST',
       body,
@@ -17,13 +19,28 @@ router.post('/', async (req, res, next) => {
       }
     }
     const createURL = `${process.env.RERUM_API_ADDR}create`
-    const result = await fetch(createURL, createOptions).then(res=>res.json())
-    .catch(err=>next(err))
+    // accomodate a JEST test attempting to test the req/resp behavior without involving RERUM
+    // https://stackoverflow.com/questions/50940640/how-to-determine-if-jest-is-running-the-code-or-not/52231746#52231746
+    // console.log(process.env.JEST_WORKER_ID)
+    // if(process.env.JEST_WORKER_ID === undefined || process.env.NODE_ENV !== 'test'){
+    //   // POST to RERUM to generate a result object
+    //   result = await fetch(createURL, createOptions)
+    //     .then(res => res.json())
+    //     .catch(err=> next(err))
+    // }
+
+    // Mock libraries hard fail on this line of code and return a headerless empty 200 response.
+    // If we want to test this file, we need to circumvent this fetch when a test is using this route.
+    result = await fetch(createURL, createOptions)
+        .then(res => res.json())
+        .catch(err=> next(err))
     res.setHeader("Location", result["@id"])
     res.status(201)
-    res.send(result)
+    console.log("CREATE ROUTE create.js sending a response")
+    res.json(result)
   }
   catch (err) {
+    console.log("CAN'T DO IT II")
     console.log(err)
     res.status(500).send("Caught Error:" + err)
   }
